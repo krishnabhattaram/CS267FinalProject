dc9dc899683006f3018854c03dfada80d0266df5
Lawrence Chen <chenyunliang12345@gmail.com>
2024-04-24 03:25:24 -0700

    add random initialization; allow fewer blocks and more nodes; cuda scaling experiments

--- gpu.cu
+++ gpu.cu
@@ -7,87 +7,106 @@
 
 #define NUM_THREADS 1024
 
-int* weights_gpu;
-int* visible_bias_gpu;
-int* hidden_bias_gpu;
+float* weights_gpu;
+float* visible_bias_gpu;
+float* hidden_bias_gpu;
 
 int* intermediate_visible_states;
 int* visible_state_gpu;
 int* hidden_state_gpu;
 curandState_t* states;
 
-__global__ void visible_to_hidden_gpu(int* visible_state, int* hidden_state, int* weights, int* visible_bias,
-                                      int* hidden_bias, int step, int n_nodes, curandState_t* states) {
-    int node_idx = blockIdx.x;
+__global__ void visible_to_hidden_gpu(int* visible_state, int* hidden_state, float* weights, float* visible_bias,
+                                      float* hidden_bias, int step, int n_nodes, int n_blocks, curandState_t* states) {
     __shared__ int dot_arr[NUM_THREADS];
-    dot_arr[threadIdx.x] = 0;
 
-    // NUM_THREADS must be greater than n_nodes!
-    if (threadIdx.x < n_nodes) {
-        dot_arr[threadIdx.x] = weights[node_idx * n_nodes + threadIdx.x] * visible_state[threadIdx.x];
-    }
-
-    __syncthreads();
-
-    // NUM_THREADS must be even!
-    // Reduce element products to final dot product
-    for (unsigned int s = 1; s < NUM_THREADS; s *= 2) {
-        if (threadIdx.x % (2 * s) == 0) {
-            dot_arr[threadIdx.x] += dot_arr[threadIdx.x + s];
+    int num_nodes_per_block = (n_nodes + n_blocks - 1) / n_blocks;
+    for (int i = 0; i < num_nodes_per_block; i++) {
+        int node_idx = i + num_nodes_per_block * blockIdx.x;
+        if (node_idx >= n_nodes) {
+            return;
         }
-        __syncthreads();
-    }
 
-    if (threadIdx.x == 0) {
-        int dot_product = dot_arr[0] + hidden_bias[node_idx];
-        // printf("BlockIdx %d Visible %d%d%d\n", blockIdx.x, visible_state[0], visible_state[1], visible_state[2]);
-        // printf("BlockIdx %d V2H Dot Product %d\n\n", blockIdx.x, dot_product);
+        // Initialize shared memory
+        dot_arr[threadIdx.x] = 0;
 
-        double sigmoid = 1. / (1 + exp(-1.0 * dot_product));
+        for (int j = 0; j < (n_nodes + NUM_THREADS - 1) / NUM_THREADS; j++) {
+            int idx = j * NUM_THREADS + threadIdx.x;
+            if (idx < n_nodes) {
+                dot_arr[threadIdx.x] += weights[node_idx * n_nodes + idx] * visible_state[idx];
+            }
+        }
 
-        float randunif = curand_uniform(&states[node_idx]);
-        hidden_state[node_idx] = (sigmoid > randunif) ? 1 : 0;
+        // __syncthreads();
+
+        // Reduction to compute final dot product
+        for (unsigned int s = 1; s < NUM_THREADS; s *= 2) {
+            int index = 2 * s * threadIdx.x;
+            if (index < NUM_THREADS) {
+                dot_arr[index] += dot_arr[index + s];
+            }
+            // __syncthreads();
+        }
+
+        if (threadIdx.x == 0) {
+            int dot_product = dot_arr[0] + hidden_bias[node_idx];
+            // printf("BlockIdx %d Visible %d%d%d\n", blockIdx.x, visible_state[0], visible_state[1], visible_state[2]);
+            // printf("BlockIdx %d V2H Dot Product %d\n\n", blockIdx.x, dot_product);
+
+            double sigmoid = 1. / (1 + exp(-1.0 * dot_product));
+
+            float randunif = curand_uniform(&states[node_idx]);
+            hidden_state[node_idx] = (sigmoid > randunif) ? 1 : 0;
+        }
     }
 
     return;
 }
 
-__global__ void hidden_to_visible_gpu(int* visible_state, int* hidden_state, int* weights, int* visible_bias,
-                                      int* hidden_bias, int step, int n_nodes, curandState_t* states, bool clamp) {
-    int node_idx = blockIdx.x;
+__global__ void hidden_to_visible_gpu(int* visible_state, int* hidden_state, float* weights, float* visible_bias,
+                                      float* hidden_bias, int step, int n_nodes, int n_blocks, curandState_t* states, bool clamp) {
     __shared__ int dot_arr[NUM_THREADS];
-    dot_arr[threadIdx.x] = 0;
 
-    // NUM_THREADS must be greater than n_nodes!
-    if (threadIdx.x < n_nodes) {
-        dot_arr[threadIdx.x] = hidden_state[threadIdx.x] * weights[threadIdx.x * n_nodes + node_idx];
-    }
-
-    __syncthreads();
-
-    // NUM_THREADS must be even!
-    // Reduce element products to final dot product
-    for (unsigned int s = 1; s < NUM_THREADS; s *= 2) {
-        if (threadIdx.x % (2 * s) == 0) {
-            dot_arr[threadIdx.x] += dot_arr[threadIdx.x + s];
+    int num_nodes_per_block = (n_nodes + n_blocks - 1) / n_blocks;
+    for (int i = 0; i < num_nodes_per_block; i++) {
+        int node_idx = i + num_nodes_per_block * blockIdx.x;
+        if (node_idx >= n_nodes) {
+            return;
         }
-        __syncthreads();
-    }
 
-    if (threadIdx.x == 0) {
-        int dot_product = dot_arr[0] + visible_bias[node_idx];
-        // printf("BlockIdx %d Hidden %d%d%d\n", blockIdx.x, hidden_state[0], hidden_state[1], hidden_state[2]);
-        // printf("BlockIdx %d H2V Final Dot Product %d\n\n", blockIdx.x, dot_product);
+        for (int j = 0; j < (n_nodes + NUM_THREADS - 1) / NUM_THREADS; j++) {
+            int idx = j * NUM_THREADS + threadIdx.x;
+            if (idx < n_nodes) {
+                dot_arr[threadIdx.x] += weights[node_idx + n_nodes * idx] * visible_state[idx];
+            }
+        }
 
-        double sigmoid = 1. / (1 + exp(-1.0 * dot_product));
+        // __syncthreads();
 
-        float randunif = curand_uniform(&states[node_idx]);
-        visible_state[node_idx] = (sigmoid > randunif) ? 1 : 0;
+        // Reduction to compute final dot product
+        for (unsigned int s = 1; s < NUM_THREADS; s *= 2) {
+            int index = 2 * s * threadIdx.x;
+            if (index < NUM_THREADS) {
+                dot_arr[index] += dot_arr[index + s];
+            }
+            // __syncthreads();
+        }
 
-        if (clamp) {
-            // Clamping
-            if (node_idx == 2) {
-                visible_state[node_idx] = 1;
+        if (threadIdx.x == 0) {
+            int dot_product = dot_arr[0] + visible_bias[node_idx];
+            // printf("BlockIdx %d Hidden %d%d%d\n", blockIdx.x, hidden_state[0], hidden_state[1], hidden_state[2]);
+            // printf("BlockIdx %d H2V Final Dot Product %d\n\n", blockIdx.x, dot_product);
+
+            double sigmoid = 1. / (1 + exp(-1.0 * dot_product));
+
+            float randunif = curand_uniform(&states[node_idx]);
+            visible_state[node_idx] = (sigmoid > randunif) ? 1 : 0;
+
+            if (clamp) {
+                // Clamping
+                if (node_idx == 2) {
+                    visible_state[node_idx] = 1;
+                }
             }
         }
     }
@@ -108,20 +127,20 @@ __global__ void randomize_visible_state(int* visible_state_gpu, int n_nodes, cur
     }
 }
 
-void init_rbm(int n_nodes, int n_weights, int n_steps, int* weights, int* visible_bias, int* hidden_bias, int seed) {
+void init_rbm(int n_nodes, int n_weights, int n_steps, float* weights, float* visible_bias, float* hidden_bias, int seed) {
     // You can use this space to initialize data objects that you may need
     // This function will be called once before the algorithm begins
     // parts live in GPU memory
     // Do not do any particle simulation here
 
-    cudaMalloc((void**)&weights_gpu, n_weights * sizeof(int));
-    cudaMemcpy(weights_gpu, weights, n_weights * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&weights_gpu, n_weights * sizeof(float));
+    cudaMemcpy(weights_gpu, weights, n_weights * sizeof(float), cudaMemcpyHostToDevice);
 
-    cudaMalloc((void**)&visible_bias_gpu, n_nodes * sizeof(int));
-    cudaMemcpy(visible_bias_gpu, visible_bias, n_nodes * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&visible_bias_gpu, n_nodes * sizeof(float));
+    cudaMemcpy(visible_bias_gpu, visible_bias, n_nodes * sizeof(float), cudaMemcpyHostToDevice);
 
-    cudaMalloc((void**)&hidden_bias_gpu, n_nodes * sizeof(int));
-    cudaMemcpy(hidden_bias_gpu, hidden_bias, n_nodes * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&hidden_bias_gpu, n_nodes * sizeof(float));
+    cudaMemcpy(hidden_bias_gpu, hidden_bias, n_nodes * sizeof(float), cudaMemcpyHostToDevice);
 
     cudaMalloc((void**)&intermediate_visible_states, n_steps * sizeof(int));
     cudaMalloc((void**)&visible_state_gpu, n_nodes * sizeof(int));
@@ -141,10 +160,10 @@ void reset_rbm(int n_nodes) {
     // print visible_state_gpu
     int* visible_state = (int*) malloc(n_nodes * sizeof(int));
     cudaMemcpy(visible_state, visible_state_gpu, n_nodes * sizeof(int), cudaMemcpyDeviceToHost);
-    for (int i = 0; i < n_nodes; i++) {
-        std::cout << visible_state[i];
-    }
-    std::cout << std::endl;
+    // for (int i = 0; i < n_nodes; i++) {
+    //     std::cout << visible_state[i];
+    // }
+    // std::cout << std::endl;
 }
 
 void free_rbm() {
@@ -158,12 +177,12 @@ void free_rbm() {
     cudaFree(states);
 }
 
-void simulate_one_step(int n_nodes, int step, int seed, int* trial_visible_vals_gpu, bool clamp) {
-    visible_to_hidden_gpu<<<n_nodes, NUM_THREADS>>>(visible_state_gpu, hidden_state_gpu, weights_gpu, 
-                                                    visible_bias_gpu, hidden_bias_gpu, step, n_nodes, states);
+void simulate_one_step(int n_blocks, int n_nodes, int step, int seed, int* trial_visible_vals_gpu, bool clamp) {
+    visible_to_hidden_gpu<<<n_blocks, NUM_THREADS>>>(visible_state_gpu, hidden_state_gpu, weights_gpu, 
+                                                    visible_bias_gpu, hidden_bias_gpu, step, n_nodes, n_blocks, states);
     cudaDeviceSynchronize();
-    hidden_to_visible_gpu<<<n_nodes, NUM_THREADS>>>(visible_state_gpu, hidden_state_gpu, weights_gpu, 
-                                                    visible_bias_gpu, hidden_bias_gpu, step, n_nodes, states, clamp);
+    hidden_to_visible_gpu<<<n_blocks, NUM_THREADS>>>(visible_state_gpu, hidden_state_gpu, weights_gpu, 
+                                                    visible_bias_gpu, hidden_bias_gpu, step, n_nodes, n_blocks, states, clamp);
     cudaDeviceSynchronize();
     
     // Log current visible node values
